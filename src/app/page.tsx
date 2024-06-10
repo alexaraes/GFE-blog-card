95c7dd5da784c8b2297688f0f9b3adbfa626f984
Alexa Schreffler <alexaschreffler@alexas-mac-mini.lan>
2024-06-10 11:26:17 -0400

    styling, broken image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import BlogCard from '../components/BlogCard';
+
+const data = {
+    category: 'Interior',
+    articleTitle: 'Top 5 Living Room Inspirations',
+    subtitle: 'Curated vibrants colors for your living, make it pop & calm in the same time.',
+    cta: 'Read more',
+    imageSrc: '/spacejoy-YqFz7UMm8qE-unsplash.jpg'
+};
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    const { category, articleTitle, subtitle, cta, imageSrc } = data;
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-[#f9fafb] to-[#d2d6db]">
+            <BlogCard
+                category={category}
+                articleTitle={articleTitle}
+                subtitle={subtitle}
+                cta={cta}
+                imageSrc={imageSrc}
+            />
+        </div>
+    )
   }
--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,8 +1,31 @@
-const BlogCard = () => {
+import ArrowIcon from './ArrowIcon';
+
+interface BlogCardProps {
+    category: string;
+    articleTitle: string;
+    subtitle: string;
+    cta: string;
+    imageSrc: string;
+};
+
+const BlogCard = ({ category, articleTitle, subtitle, cta, imageSrc }: BlogCardProps) => {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center w-72 space-y-1.5 bg-white">
             <div>
-                <img src='' />
+                <img src={imageSrc} />
+                <div className="p-3">
+                    <span className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-teal-100 text-teal-800">{category}</span>
+                </div>
+                <div className="text-lg px-3">
+                    <p>{articleTitle}</p>
+                </div>
+                <div className="text-base text-neutral-600 px-3">
+                    <p>{subtitle}</p>
+                </div>
+                <div className="flex flex-row justify-start items-center p-3">
+                    <a className="text-indigo-700">{cta}</a>
+                    <ArrowIcon />
+                </div>
             </div>
         </div>
     )
